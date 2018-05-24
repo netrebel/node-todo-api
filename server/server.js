4b4674897d34b6d1f192f10a77d06aecdde36510
Miguel Reyes <miguel.reyes@enernoc.com>
2018-05-24 17:08:35 -0400

    Validatords, types and defaults

--- server/server.js
+++ server/server.js
@@ -5,22 +5,27 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default : false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default : null
   }
 });
 
 var newTodo = new Todo({
-  text: 'Cook dinner'
+  text: ' Edit this video '
 })
 
 newTodo.save().then((doc) => {
   console.log('Saved todo', doc);
 }, (e) => {
-  console.log('Unable to save todo');
+  console.log('Unable to save todo: ', e);
 });
