4eb2a671de5ff97ec5a90285b0261a0f1bacea36
Miguel Reyes <miguel.reyes@enernoc.com>
2018-06-12 12:54:38 -0400

    Add POST /users
    $ npm install validator --save

--- server/server.js
+++ server/server.js
@@ -97,6 +97,18 @@ app.patch('/todos/:id', (req, res) => {
 
 });
 
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+    user.save().then((user) => {
+        console.log('saved: ' + JSON.stringify(user));
+        res.send(user);
+    }).catch((e) => {
+      res.status(400).send(e);
+    });
+
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
