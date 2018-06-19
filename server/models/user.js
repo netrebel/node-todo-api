458842bfaede352be0f771b6abf166e6935dc331
Miguel Reyes <miguel.reyes@enernoc.com>
2018-06-19 16:52:12 -0400

    GET /users/me with jwt.verify

--- server/models/user.js
+++ server/models/user.js
@@ -39,6 +39,7 @@ UserSchema.methods.toJSON = function () {
   return _.pick(userObject, ['_id', 'email']);
 };
 
+//Instance methods are defined calling 'methods'
 UserSchema.methods.generateAuthToken = function () {
   var user = this;
   var access = 'auth';
@@ -52,6 +53,27 @@ UserSchema.methods.generateAuthToken = function () {
     return token;
   });
 }
+
+// Model method is defined calling 'statics'
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    // return new Promies((resolve, reject) =>{
+    //   reject();
+    // });
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    _id: decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth',
+  });
+};
 
 var User = mongoose.model('User', UserSchema);
 
