a721abad1e8f3e1962130b81cafecba010c5fb5d
Miguel Reyes <miguel.reyes@enernoc.com>
2018-09-10 16:21:56 -0400

    Update config to use JSON file

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; //Tell mongoose what promise library to use
+console.log(process.env.MONGODB_URI);
+
 mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose}
